10eff9ace6bc36e1e89955959efca0af1b1bb51e
AntonAzer <tonyazer86@gmail.com>
2025-11-20 15:42:19 +0200

    Update Input.h

--- Input.h
+++ Input.h
@@ -19,16 +19,17 @@ public:
 	Input(window* pW);  // Constructor أنا عاملها
 
 	// Basic input functions
-	void GetPointClicked(Point& P) const;      // Get mouse click coordinates أنا عاملها
-	string GetString(Output* pO) const;        // Get a string input from the user أنا عاملها
+	void GetPointClicked(Point& P) const;     
+	string GetString(Output* pO) const;       
 	double GetValue(Output* pO) const;         // Get a numeric value (double) أنا عاملها
-	string GetVariable(Output* pO) const;      // Get a valid variable name أنا عاملها
+	string GetVariable(Output* pO) const;      // Get a valid variable name أنا مكملها
 	char GetArithOperator(Output* pO) const;   // Get an arithmetic operator (+,-,*,/) أنا عاملها
 	string GetCompOperator(Output* pO) const;  // Get a comparison operator (==, !=, <, <=, >, >=) أنا عاملها
 
 	ActionType GetUserAction() const;  // Map user click to action أنا عاملها
 
-	~Input();  // Destructor أنا عاملها
+	~Input();  // Destructor مفيهاش محتوي 
 };
 
 #endif
+
