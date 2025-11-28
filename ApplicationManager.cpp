3f733de4fb38a7fa5c94a709da911ca075bbb6b8
B-Mores <beshoymoress777@gmail.com>
2025-11-28 19:05:44 +0200

    Update ApplicationManager.cpp

--- ApplicationManager.cpp
+++ ApplicationManager.cpp
@@ -1,5 +1,9 @@
 #include "ApplicationManager.h"
 #include "Actions\AddValueAssign.h"
+#include "Actions\AddVariableAssign.h"
+#include "Actions\AddOperationAssign.h"
+#include "Actions\AddRead.h"
+#include "Actions\AddWrite.h"
 #include "Actions\AddStart.h"
 #include "Actions\AddEnd.h"
 #include "GUI\Input.h"
@@ -11,42 +15,18 @@ ApplicationManager::ApplicationManager()
 	//Create Input and output
 	pOut = new Output;
 	pIn = pOut->CreateInput();
-
+	
 	StatCount = 0;
 	ConnCount = 0;
 	pSelectedStat = NULL;	//no Statement is selected yet
 	pClipboard = NULL;
-
+	
 	//Create an array of Statement pointers and set them to NULL		
-	for (int i = 0; i < MaxCount; i++)
+	for(int i=0; i<MaxCount; i++)
 	{
-		StatList[i] = NULL;
+		StatList[i] = NULL;	
 		ConnList[i] = NULL;
 	}
-}
-
-//Destructor: free allocated resources
-ApplicationManager::~ApplicationManager()
-{
-	// Delete all statements
-	for (int i = 0; i < StatCount; ++i)
-	{
-		delete StatList[i];
-		StatList[i] = NULL;
-	}
-	StatCount = 0;
-
-	// Delete all connectors (if any)
-	for (int i = 0; i < ConnCount; ++i)
-	{
-		delete ConnList[i];
-		ConnList[i] = NULL;
-	}
-	ConnCount = 0;
-
-	// Delete input and output objects
-	delete pIn; pIn = NULL;
-	delete pOut; pOut = NULL;
 }
 
 
@@ -58,50 +38,70 @@ ApplicationManager::~ApplicationManager()
 ActionType ApplicationManager::GetUserAction() const
 {
 	//Ask the input to get the action from the user.
-	return pIn->GetUserAction();
+	return pIn->GetUserAction();		
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //Creates an action and executes it
-void ApplicationManager::ExecuteAction(ActionType ActType)
+void ApplicationManager::ExecuteAction(ActionType ActType) 
 {
 	Action* pAct = NULL;
-
+	
 	//According to ActioType, create the corresponding action object
 	switch (ActType)
 	{
-	case ADD_VALUE_ASSIGN:
-		pAct = new AddValueAssign(this);
-		break;
+		case ADD_VALUE_ASSIGN:
+			pAct = new AddValueAssign(this);
+			break;
+	
+		case ADD_VAR_ASSIGN:
+			pAct = new AddVariableAssign(this);
+			break;
 
-	case ADD_START:
-		pAct = new AddStart(this);
-		break;
 
-	case ADD_END:
-		pAct = new AddEnd(this);
-		break;
+		case ADD_OPER_ASSIGN:
+			pAct = new AddOperationAssign(this);
+			break;
 
-	case ADD_CONDITION:
-		///create AddCondition Action here
+		case ADD_READ:
+			pAct = new AddRead(this);
+			break;
 
-		break;
+		case ADD_WRITE:
+			pAct = new AddWrite(this);
+			break;
 
-	case SELECT:
-		///create Select Action here
+		case ADD_START:
+			pAct = new AddStart(this);
+			break;
 
-		break;
+		case ADD_END:
+			pAct = new AddEnd(this);
+			break;
 
-	case EXIT:
-		///create Exit Action here
 
-		break;
 
-	case STATUS:
-		return;
+			
+		case ADD_CONDITION:
+			///create AddCondition Action here
+
+			break;
+
+		case SELECT:
+			///create Select Action here
+
+			break;
+
+		case EXIT:
+			///create Exit Action here
+			
+			break;
+		
+		case STATUS:
+			return;
 	}
-
+	
 	//Execute the created action
-	if (pAct != NULL)
+	if(pAct != NULL)
 	{
 		pAct->Execute();//Execute
 		delete pAct;	//Action is not needed any more ==> delete it
@@ -109,74 +109,90 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 }
 
 
+
 //==================================================================================//
 //						Statements Management Functions								//
 //==================================================================================//
 
+
 //Add a statement to the list of statements
-void ApplicationManager::AddStatement(Statement* pStat)
+void ApplicationManager::AddStatement(Statement *pStat)
 {
-	if (StatCount < MaxCount)
+	if(StatCount < MaxCount)
 		StatList[StatCount++] = pStat;
+	
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-//Return the selected statement
-Statement* ApplicationManager::GetSelectedStatement() const
+Statement *ApplicationManager::GetStatement(Point P) const
 {
-	return pSelectedStat;
+	//If this point P(x,y) belongs to a statement return a pointer to it.
+	//otherwise, return NULL
+
+
+	///Add your code here to search for a statement given a point P(x,y)	
+	///WITHOUT breaking class responsibilities
+
+	return NULL;
 }
+////////////////////////////////////////////////////////////////////////////////////
+//Returns the selected statement
+Statement *ApplicationManager::GetSelectedStatement() const
+{	return pSelectedStat;	}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Set the statement selected by the user
-void ApplicationManager::SetSelectedStatement(Statement* pStat)
-{
-	pSelectedStat = pStat;
-}
+void ApplicationManager::SetSelectedStatement(Statement *pStat)
+{	pSelectedStat = pStat;	}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Returns the Clipboard
-Statement* ApplicationManager::GetClipboard() const
-{
-	return pClipboard;
-}
+Statement *ApplicationManager::GetClipboard() const
+{	return pClipboard;	}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Set the Clipboard
-void ApplicationManager::SetClipboard(Statement* pStat)
-{
-	pClipboard = pStat;
-}
+void ApplicationManager::SetClipboard(Statement *pStat)
+{	pClipboard = pStat;	}
 
 
-////////////////////////////////////////////////////////////////////////////////////
-// Return pointer to the input
-Input* ApplicationManager::GetInput() const
-{
-	return pIn;
-}
+//==================================================================================//
+//							Interface Management Functions							//
+//==================================================================================//
 
-////////////////////////////////////////////////////////////////////////////////////
-// Return pointer to the output
-Output* ApplicationManager::GetOutput() const
-{
-	return pOut;
-}
 
-////////////////////////////////////////////////////////////////////////////////////
-// Redraws all the drawing window
+//Draw all figures on the user interface
 void ApplicationManager::UpdateInterface() const
 {
-	// Clear drawing area first
-	if (pOut)
-		pOut->ClearDrawArea();
+	pOut->ClearDrawArea();
 
-	// Draw all statements
-	for (int i = 0; i < StatCount; ++i)
-	{
-		if (StatList[i] != NULL)
-			StatList[i]->Draw(pOut);
-	}
+	//Draw all statements
+	for(int i=0; i<StatCount; i++)
+		StatList[i]->Draw(pOut);
+	
+	//Draw all connections
+	for(int i=0; i<ConnCount; i++)
+		ConnList[i]->Draw(pOut);
 
-	// If you have connectors drawing implemented later, draw them here.
+}
+////////////////////////////////////////////////////////////////////////////////////
+//Return a pointer to the input
+Input *ApplicationManager::GetInput() const
+{	return pIn; }
+//Return a pointer to the output
+Output *ApplicationManager::GetOutput() const
+{	return pOut; }
+////////////////////////////////////////////////////////////////////////////////////
+
+
+//Destructor
+ApplicationManager::~ApplicationManager()
+{
+	for(int i=0; i<StatCount; i++)
+		delete StatList[i];
+	for(int i=0; i<StatCount; i++)
+		delete ConnList[i];
+	delete pIn;
+	delete pOut;
+	
 }
