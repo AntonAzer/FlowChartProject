f4111cfb8bfb92c31b0ab462cc5634c8e9813e98
B-Mores <beshoymoress777@gmail.com>
2025-11-28 18:42:25 +0200

    Update Write.h

--- Write.h
+++ Write.h
@@ -1,1 +1,32 @@
+#ifndef WRITE_H
+#define WRITE_H
 
+#include "Statement.h"
+
+
+class Write : public Statement
+{
+private:
+	string var;
+
+	Connector* pOutConn;	//Read Stat. has one Connector to next statement
+
+	Point Inlet;	//A point where connections enters this statement 
+	//It's used as the (End) point of the (Input) connectors
+	Point Outlet;	//A point a connection leaves this statement
+	//It's used as the (Start) point of the (Output) connector
+
+	Point LeftCorner;	//left corenr of the statement block.
+
+	virtual void UpdateStatementText();
+
+public:
+	Write(Point Lcorner, string variable = "");
+
+	void setVar(const string& Variable);
+
+	virtual void Draw(Output* pOut) const;
+
+};
+
+#endif
